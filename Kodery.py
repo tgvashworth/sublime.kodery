4c0745a08b21f21d4b51941b024593ad59e88160
Tom Ashworth <tom@phuu.net>
2013-08-28 11:52:31 +0100

    Clean up and document somewhat.

--- Kodery.py
+++ Kodery.py
@@ -3,54 +3,70 @@ import sublime, sublime_plugin
 import json
 from urllib import urlopen
 
-kodery_url = "http://api.kodery.com/me/snippets?access_token="
 
 class Kodery(sublime_plugin.EventListener):
-  fragments = []
+  url = "http://api.kodery.com/me/snippets?access_token="
+  completions = []
 
   @staticmethod
   def reload():
     sublime.status_message('Reloading Kodery snippets...')
 
     # Load the current settings
-    settings = sublime.load_settings("Preferences.sublime-settings")
-    kodery = settings.get('kodery', {})
+    settings = sublime.load_settings("Preferences.sublime-settings").get('kodery', {})
 
     # If the token is missing, warn and quit.
-    if 'token' not in kodery:
+    if 'token' not in settings:
       sublime.status_message('You are missing a Kodery token in your Sublime preferences.')
       return
 
     # Try to load the snippets, and warn if something goes wrong
     try:
-      snippets = json.loads(urlopen(kodery_url + kodery.get('token', "")).read())
+      snippets = json.loads(urlopen(Kodery.url + settings.get('token', "")).read())
     except IOError:
       sublime.status_message('Your Kodery token is not valid. Generate a new one at account.kodery.com.')
       return
 
     # Build a list of fragments
-    fragments = []
+    completions = []
     for snippet in snippets:
-      for fragment in snippet['fragments']:
-        if not fragment['name'] and len(snippet['fragments']) == 1:
-          fragment['name'] = snippet['name']
-        fragment['snippet'] = snippet
-        fragments.append(fragment)
-    Kodery.fragments = fragments
+      # Append the completion with all fragments using the snippet name
+      # completions.append({
+      #   'name': snippet['name'],
+      #   'description': snippet['category']['name'],
+      #   'body': '\n\n'.join([fragment['body'] for fragment in snippet['fragments']])
+      # })
 
-    sublime.status_message('Kodery snippets reloaded.')
+      # Add individual fragments as completions
+      for fragment in snippet['fragments']:
+        if not fragment['name']:
+          if len(snippet['fragments']) == 1:
+            fragment['name'] = snippet['name']
+          else:
+            continue
+        # Append the completion
+        completions.append({
+          'name': fragment['name'],
+          'description': snippet['name'],
+          'body': fragment['body']
+        })
+
+    # Save the completions as static class property
+    Kodery.completions = completions
+
+    sublime.status_message('Your Kodery were snippets reloaded.')
 
   def __init__(self):
     Kodery.reload()
 
   def on_query_completions(self, view, prefix, locations):
-    # Find the matching fragments and pass them back
-    fragments = []
-    for fragment in Kodery.fragments:
-      if fragment['name'].lower().startswith(prefix.lower()):
-        trigger = fragment['name'] + '\t' + fragment['snippet']['name']
-        fragments.append((trigger, fragment['body']))
-    return fragments
+    # Find the matching completions and pass them back
+    completions = []
+    for completion in Kodery.completions:
+      if completion['name'].lower().startswith(prefix.lower()):
+        trigger = completion['name'] + '\t' + completion['description']
+        completions.append((trigger, completion['body']))
+    return completions
 
 class ReloadKoderySnippetsCommand(sublime_plugin.WindowCommand):
   def run(self):
